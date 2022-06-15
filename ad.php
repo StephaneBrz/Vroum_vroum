627a740367d5f4b6535d4b0c0d4f7eb99696c3b1
davidathuil <david.athuil@gmail.com>
2022-06-15 14:32:20 +0200

    formulaire

---
+++ ad.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Import */
+require_once __DIR__ . "/lib/db.php";
+
+/* Si le verbe HTTP est différent de POST */
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    http_response_code(405);
+    die();
+}
+
+/* Récupération des données du formulaire */
+$title = htmlspecialchars($_POST["title"]);
+$description = htmlspecialchars($_POST["description"]);
+$beginprice = htmlspecialchars($_POST["beginprice"]);
+$reserveprice = htmlspecialchars($_POST["reserveprice"]);
+$enddate = htmlspecialchars($_POST["enddate"]);
+$model = htmlspecialchars($_POST["model"]);
+$brand = htmlspecialchars($_POST["brand"]);
+$power = htmlspecialchars($_POST["power"]);
+$year = htmlspecialchars($_POST["year"]);
+
+/* Préparation de la requête */
+$query = $dbh->prepare("INSERT INTO ad (title,description,beginprice,	reserveprice ,enddate ,	model ,	brand ,	power, 	year) VALUES (?, ?, ?, ?, ?,?,?);");
+
+/* Exécution de la requête */
+/* On obtient une valeur de résultat indiquant le nombre de lignes affectées par la requête */
+$result = $query->execute([$title, $description, $beginprice, $reserveprice, $enddate, $model, $brand, $power, $year]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Form</title>
+</head>
+
+<body>
+    <?php if ($result == 1) { ?>
+        <p>Merci de votre message</p>
+    <?php } else { ?>
+        <p>Une erreur s'est produite, veuillez réessayer</p>
+    <?php } ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VRoum VRoum</title>
+</head>
+
+<body>
+    <h2>Creation Annonce</h2>
+    <form action="ad.php" method="POST">
+        <label>title</label>
+        <input type=" text" name="title">
+
+        <label>description</label>
+        <input type="text" name="description">
+
+        <label>beginprice</label>
+        <input type="number" name="beginprice" step="0.01">
+        <label>reserveprice</label>
+        <input type="number" name="reserveprice step=" 0.01">
+        <label>enddate</label>
+        <input type="text" name="message">
+        <label>model</label>
+        <input type="text" name="model">
+        <label>brand</label>
+        <input type="text" name="brand">
+        <label>year</label>
+        <input type="number" name="year" min="1910" max="2022">
+
+
+        <input type="submit" value="Register">
+    </form>
+
+    <h2>Search annonce</h2>
+    <form action="customer-info.php" method="post">
+        <input type="text" name="email" />
+        <input type="submit" value="search">
+    </form>
+</body>
+
+</html>
